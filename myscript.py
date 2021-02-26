ea8bfe875723244ce4d1d051f1d1903276d79e9e
Arturo <arturbutkiewicz@gmail.com>
2021-02-26 13:33:31 +0100

    sdsd

--- myscript.py
+++ myscript.py
@@ -6,5 +6,9 @@ board = RocketBoard(2)
 print(board[0])
 
 class Sample:
-    def __init__(self):
+    def __init__(self, isSucces, value):
+        self.isSucces = isSucces
+        self.value = value
+
+    def backup ():
         pass
